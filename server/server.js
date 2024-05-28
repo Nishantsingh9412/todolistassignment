69bbfaa57506858f163457dc5c3740c0317f2258
nishant <59526404+Nishantsingh9412@users.noreply.github.com>
2024-05-28 16:07:34 +0530

    basic setup

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from 'cors';
+import dotenv from 'dotenv'
+
+const app = express()
+dotenv.config()
+
+const PORT = 5000
+const DATABASE_URL = process.env.DATABASE_URL
+
+
+app.use(express.json({limit:'30mb',extended:true}));
+app.use(express.urlencoded({limit:'30mb',extended:'true'}))
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+  res.send(' This is TodoList Server ')
+})
+
+mongoose.connect(DATABASE_URL , {useNewUrlParser:true,useUnifiedTopology:true})
+.then(
+    () => app.listen(PORT, 
+        () => {console.log(`server running on PORT ${PORT}`)})
+).catch(
+    (err) => console.log(err)
+);
